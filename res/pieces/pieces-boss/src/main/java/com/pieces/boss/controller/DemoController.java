a11d0496584848a3ee99e03486f01726bfb93487
wangbin <burgleaf@gmail.com>
2016-07-01 18:30:03 +0800

    添加分页标签，和分页demo

---
+++ res/pieces/pieces-boss/src/main/java/com/pieces/boss/controller/DemoController.java
@@ -0,0 +1,56 @@
+package com.pieces.boss.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.pieces.dao.model.Area;
+import com.pieces.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by wangbin on 2016/7/1.
+ */
+@Controller
+@RequestMapping(value = "demo")
+public class DemoController {
+
+    @Autowired
+    private AreaService areaService;
+
+    /**
+     * 测试分页
+     * @param request
+     * @param response
+     * @param pageNum
+     * @param pageSize
+     * @param modelMap
+     * @return
+     */
+    @RequestMapping("page")
+    public String index(HttpServletRequest request,
+                        HttpServletResponse response,
+                        Integer pageNum,
+                        Integer pageSize,
+                        ModelMap modelMap){
+
+        if(pageNum==null){
+            pageNum = 1;
+        }
+        if(pageSize==null){
+            pageSize=10;
+        }
+
+        PageInfo<Area> areaPage =  areaService.findByPage(1,pageNum,pageSize);
+
+        modelMap.put("areaPage",areaPage);
+        return "customers_test";
+    }
+
+
+
+
+}
---
+++ res/pieces/pieces-boss/src/main/webapp/public/inc/meta.ftl
@@ -0,0 +1,10 @@
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<base href="http://localhost:81"/>
+<link type="text/css" rel="stylesheet" href="/css/style.css" />
+<#--<link rel="shortcut icon" href="/assets/favicon.ico" />-->
+<!--[if lt IE 9]>
+<script src="html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
