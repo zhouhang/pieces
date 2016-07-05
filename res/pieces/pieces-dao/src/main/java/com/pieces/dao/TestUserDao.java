474753d8f76c5f0f148909fe69ecaaef59f15d20
wangbin <burgleaf@gmail.com>
2016-07-05 11:43:34 +0800

    删除TestUser相关代码

--- res/pieces/pieces-dao/src/main/java/com/pieces/dao/TestUserDao.java
+++
@@ -1,7 +0,0 @@
-package com.pieces.dao;
-
-import com.pieces.dao.model.TestUser;
-
-public interface TestUserDao {
-	TestUser getTestUserByUserName(String userName);
-}
--- res/pieces/pieces-service/src/main/java/com/pieces/service/TestUserService.java
+++
@@ -1,7 +0,0 @@
-package com.pieces.service;
-
-import com.pieces.dao.model.TestUser;
-
-public interface TestUserService {
-	TestUser getTestUserByUserName(String userName);
-}
