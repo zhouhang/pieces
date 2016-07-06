623e9aba64274f1506b5683faae263c6c20a0324
wangbin <burgleaf@gmail.com>
2016-07-06 18:36:35 +0800

    提交BOSS全局异常

---
+++ res/pieces/pieces-boss/src/main/java/com/pieces/boss/controller/exception/BaseGlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.pieces.boss.controller.exception;
+
+import com.google.common.base.Throwables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * Created by wangbin on 2016/6/30.
+ */
+public class BaseGlobalExceptionHandler {
+
+    protected static final Logger logger = null;
+    protected static final String DEFAULT_ERROR_MESSAGE = "系统忙，请稍后再试";
+
+    protected ModelAndView handleError(HttpServletRequest req, HttpServletResponse rsp, Exception e, String viewName, HttpStatus status) throws Exception {
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null){
+            throw e;
+        }
+
+        String errorMsg =  DEFAULT_ERROR_MESSAGE;
+        String errorStack = Throwables.getStackTraceAsString(e);
+
+        getLogger().error("Request: {} raised {}", req.getRequestURI(), errorStack);
+//        if (Ajax.isAjax(req)) {
+//            return handleAjaxError(rsp, errorMsg, status);
+//        }
+        return handleViewError(req.getRequestURL().toString(), errorStack, errorMsg, viewName);
+    }
+
+    protected ModelAndView handleViewError(String url, String errorStack, String errorMessage, String viewName) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", errorStack);
+        mav.addObject("url", url);
+        mav.addObject("message", errorMessage);
+        mav.addObject("timestamp", new Date());
+        mav.setViewName(viewName);
+        return mav;
+    }
+
+    protected ModelAndView handleAjaxError(HttpServletResponse rsp, String errorMessage, HttpStatus status) throws IOException {
+        rsp.setCharacterEncoding("UTF-8");
+        rsp.setStatus(status.value());
+        PrintWriter writer = rsp.getWriter();
+        writer.write(errorMessage);
+        writer.flush();
+        return null;
+    }
+
+    public Logger getLogger() {
+        return LoggerFactory.getLogger(BaseGlobalExceptionHandler.class);
+    }
+}
---
+++ res/pieces/pieces-boss/src/main/java/com/pieces/boss/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.pieces.boss.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by wangbin on 2016/6/30.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler  extends BaseGlobalExceptionHandler{
+
+
+
+
+    //500的异常会被这个方法捕获
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView handleError(HttpServletRequest req, HttpServletResponse rsp, Exception e) throws Exception {
+        return handleError(req, rsp, e, "error-front", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+
+    @Override
+    public Logger getLogger() {
+        return LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    }
+
+}
