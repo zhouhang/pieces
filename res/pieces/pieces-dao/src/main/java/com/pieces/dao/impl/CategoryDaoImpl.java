9dc2835873cb6a54c0aa569a74cdabdaf83ee0b5
feng-bene <ysflewup@163.com>
2016-07-12 15:46:43 +0800

    商品模块：model，mapper，dao，daoimpl

---
+++ res/pieces/pieces-dao/src/main/java/com/pieces/dao/impl/CategoryDaoImpl.java
@@ -0,0 +1,48 @@
+package com.pieces.dao.impl;
+
+import com.github.pagehelper.PageInfo;
+import com.pieces.dao.CategoryDao;
+import com.pieces.dao.model.Category;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public class CategoryDaoImpl extends BaseDaoImpl implements CategoryDao{
+
+        @Override
+        public Category findById(int id) {
+            return getSqlSession().selectOne("com.pieces.dao.CategoryMapper.findById", id);
+        }
+
+
+        @Override
+        public List<Category> findAll() {
+            return getSqlSession().selectList("com.pieces.dao.CategoryMapper.findAll");
+        }
+
+        @Override
+        public PageInfo<Category> find(int pageNum, int pageSize) {
+            List<Category> list = getSqlSession().selectList("com.pieces.dao.CategoryMapper.findAll", null,new RowBounds(pageNum, pageSize));
+            PageInfo page = new PageInfo(list);
+            return page;
+        }
+
+        @Override
+        public int deleteById(int id) {
+            return getSqlSession().delete("com.pieces.dao.CategoryMapper.deleteById",id);
+        }
+
+        @Override
+        public int create(Category category) {
+            return getSqlSession().insert("com.pieces.dao.CategoryMapper.create",category);
+        }
+
+        @Override
+        public int update(Category category) {
+            return getSqlSession().update("com.pieces.dao.CategoryMapper.update",category);
+        }
+
+}
---
+++ res/pieces/pieces-dao/src/main/java/com/pieces/dao/model/Code.java
@@ -0,0 +1,89 @@
+package com.pieces.dao.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+
+public class Code  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	//自定义的Code值
+	private String code;
+	
+	//显示值
+	private String name;
+	
+	//关联分类品种ID
+	private Integer relatedCode;
+	
+	//记录状态 -1 删除, 0 禁用, 1 启用
+	private Integer status;
+	
+	//Code 类型: 例如 切制规格, 原药产地..... 等
+	private Integer typeId;
+	
+	private Date createTime;
+	
+	public Code(){}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getRelatedCode() {
+		return relatedCode;
+	}
+
+	public void setRelatedCode(Integer relatedCode) {
+		this.relatedCode = relatedCode;
+	}
+	
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	public Integer getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(Integer typeId) {
+		this.typeId = typeId;
+	}
+	
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+}
