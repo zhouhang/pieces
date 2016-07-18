8de441b2c1901ed275cdf0b77af193eef4df0497
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2016-07-18 09:37:16 +0800

    [Summary] 移除merger 错误的方法声明.

--- res/pieces/pieces-dao/src/main/java/com/pieces/dao/CommodityDao.java
+++ res/pieces/pieces-dao/src/main/java/com/pieces/dao/CommodityDao.java
@@ -6,15 +6,7 @@ import com.pieces.dao.vo.CommodityVO;
 
 public interface CommodityDao extends ICommonDao<Commodity>{
 
-    /**
-     * 根据参数获查询商品信息
-     * @param commodity
-     * @param pageNum
-     * @param pageSize
-     * @return
-     */
-    public PageInfo<Commodity> findByParam (Commodity commodity, int pageNum, int pageSize);
-
     public PageInfo<CommodityVO> findVoByPage(int pageNum, int pageSize);
+
     public PageInfo<CommodityVO> findByParam (CommodityVO commodity, int pageNum, int pageSize);
 }
