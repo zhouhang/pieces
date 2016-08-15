df288d668e4ab1b69b8f32d0b62dedb5c3d39833
feng-bene <ysflewup@163.com>
2016-08-15 14:29:18 +0800

    优化

--- res/pieces/pieces-dao/src/main/java/com/pieces/dao/vo/CategoryVo.java
+++ res/pieces/pieces-dao/src/main/java/com/pieces/dao/vo/CategoryVo.java
@@ -37,20 +37,4 @@ public class CategoryVo extends Category{
 	public void setCategoryPinyin(Map<String, List<CategoryVo>> categoryPinyin) {
 		this.categoryPinyin = categoryPinyin;
 	}
-
-	@Override
-    public String toString() {
-        Field[] fields = this.getClass().getDeclaredFields();
-        StringBuffer sb = new StringBuffer();
-        try {
-            for(Field field : fields){
-                if(field.get(this)!=null){
-                    sb.append("&").append(field.getName()).append("=").append(field.get(this).toString());
-                }
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
 }
