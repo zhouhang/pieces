07733f9918c8e5b2fd046c44a7ff74e6cebde89f
frank <frank1989678@126.com>
2016-08-29 11:40:00 +0800

    静态文件更新

---
+++ res/pieces/pieces-boss/src/main/webapp/assets/js/notify.js
@@ -0,0 +1,53 @@
+;(function($){
+	var defaults = {
+		clickToHide: true 	// 点击关闭
+		,delay: 5e3 		// 5秒后自动关闭，为0时不关闭
+		,title: '提示消息' 	// 文字
+		,text: '' 			// 说明
+		,type: 'warn' 		// 类型：错误(error)，正确(success)，警告(warn)
+		,call: null 		// 关闭后回调
+	}
+
+	var icons = {
+		error: '<i class="fa fa-times-circle"></i>'
+		,success: '<i class="fa fa-check-circle"></i>'
+		,warn: '<i class="fa fa-prompt"></i>'
+	}
+	var $wrapper = $('<div class="notify-wrapper" />').appendTo($('body'));
+
+	$.notify = function(options) {
+		var settings = $.extend({}, defaults, options);
+		var modal = [];
+		modal.push('<div class="message ', settings.type, settings.clickToHide ? ' hidable' : '', '">');
+		modal.push(	   '<div class="inner">');
+		modal.push(	       '<div class="icon">');
+		modal.push(	           icons[settings.type]);
+		modal.push(	       '</div>');
+		modal.push(	       '<div class="text', (settings.text ? '' : ' mid') , '">');
+		modal.push(	           '<h3>', settings.title, '</h3>');
+		modal.push(	           '<p>', settings.text, '</p>');
+		modal.push(	       '</div>');
+		modal.push(	   '</div>');
+		modal.push('</div>');
+
+		var $modal = $(modal.join(''));
+		$wrapper.prepend($modal);
+		if (settings.delay === 0) {
+			$modal.slideDown(400);
+
+		} else {
+			$modal.slideDown(400).delay(settings.delay).slideUp(200, function() {
+				$modal.remove();			
+			});
+		}
+		typeof options.call === 'function' && options.call();
+	};
+
+	// 点击关闭
+	$('body').on('click', '.hidable', function() {
+		var $self = $(this);
+		$self.stop().slideUp(200, function() {
+			$self.remove();
+		});
+	})
+})(jQuery)
