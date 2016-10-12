e2951b3f8c9f015d27f055b0eecf155ff5361723
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2016-10-12 14:50:24 +0800

    [Summary] 初始化配置

---
+++ res/medicine-store/ms-tools/src/main/java/com/ms/tools/entity/ResultStatus.java
@@ -0,0 +1,16 @@
+package com.ms.tools.entity;
+
+/**
+ * Author: koabs
+ * 8/22/16.
+ * 返回结果状态
+ */
+public class ResultStatus {
+    // 用户未登入
+    public static Integer Not_Login = -1;
+
+    public static Integer Error = 500;
+
+    public static Integer Success = 200;
+
+}
