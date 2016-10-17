01922a6b2943d3c0235fdaa5d762594065815eb4
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2016-10-17 11:35:13 +0800

    [Summary] 初始化

--- res/yaoyy/ms-boss/src/main/java/com/ms/boss/controller/AdminController.java
+++ res/yaoyy/ms-boss/src/main/java/com/ms/boss/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.ms.boss.controller;
 
+import com.ms.dao.model.Admin;
+import com.ms.tools.entity.Result;
+
 /**
  * Author: koabs
  * 10/13/16.
@@ -11,4 +14,35 @@ public class AdminController {
     // 删除
     // list
     // 详情
+
+    /**
+     *  登入
+     * @param username
+     * @param password
+     * @return
+     */
+    public Result login(String username, String password){
+        return null;
+    }
+
+    /**
+     * 登出
+     * @return
+     */
+    public Result loginOut() {
+        return null;
+    }
+
+    /**
+     * 保存
+     * @param admin
+     * @return
+     */
+    public Result save(Admin admin) {
+        return null;
+    }
+
+
+
+
 }
