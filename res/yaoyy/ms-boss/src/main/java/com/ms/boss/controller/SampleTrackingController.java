f0e37a14cb4729e096456bf705fc436e119888cd
wangbin <burgleaf@gmail.com>
2016-10-24 10:20:52 +0800

    添加管理员后台界面

--- res/yaoyy/ms-boss/src/main/java/com/ms/boss/controller/SampleTrackingController.java
+++ res/yaoyy/ms-boss/src/main/java/com/ms/boss/controller/SampleTrackingController.java
@@ -1,20 +1,16 @@
 package com.ms.boss.controller;
 
-import com.github.pagehelper.PageInfo;
 import com.ms.dao.model.SampleTracking;
 import com.ms.dao.model.TrackingDetail;
-import com.ms.dao.vo.SampleTrackingVo;
 import com.ms.service.SampleTrackingService;
 import com.ms.service.TrackingDetailService;
 import com.ms.tools.entity.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 
 /**
  * Created by xiao on 2016/10/18.
