bdb29181bedb710ff3bbc3d688ba97559688c099
2011zhouhang@gmail.com <2011zhouhang@gmail.com>
2016-10-26 11:22:32 +0800

    8188 for biz and 8189 for boss

--- res/yaoyy/ms-biz/src/main/java/com/ms/Application.java
+++ res/yaoyy/ms-biz/src/main/java/com/ms/Application.java
@@ -2,18 +2,24 @@ package com.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by burgl on 2016/8/13.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8188);
+    }
 
 }
