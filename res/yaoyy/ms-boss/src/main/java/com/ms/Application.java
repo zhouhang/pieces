4229cc309985a96a5bb2561ab285eba99d981add
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2016-11-04 10:22:44 +0800

    [Summary]后台项目启动优化

--- res/yaoyy/ms-boss/src/main/java/com/ms/Application.java
+++ res/yaoyy/ms-boss/src/main/java/com/ms/Application.java
@@ -2,9 +2,13 @@ package com.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * Created by burgl on 2016/8/13.
@@ -17,9 +21,28 @@ public class Application extends SpringBootServletInitializer implements Embedde
         SpringApplication.run(Application.class, args);
     }
 
+    public Application() {
+        super();
+        setRegisterErrorPageFilter(false); // <- this one
+    }
+
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
         container.setPort(8189);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    //显示声明CommonsMultipartResolver为mutipartResolver
+    @Bean(name = "multipartResolver")
+    public MultipartResolver multipartResolver(){
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("UTF-8");
+        resolver.setResolveLazily(true);//resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常
+        resolver.setMaxInMemorySize(40960);
+        resolver.setMaxUploadSize(50*1024*1024);//上传文件大小 50M 50*1024*1024
+        return resolver;
+    }
 }
