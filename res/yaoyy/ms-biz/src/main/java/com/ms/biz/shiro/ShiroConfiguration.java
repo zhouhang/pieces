ed721cc3c08976678a68f2ae3540ad2e9502940f
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2016-11-07 15:20:10 +0800

    [Summary]

--- res/yaoyy/ms-biz/src/main/java/com/ms/biz/shiro/ShiroConfiguration.java
+++ res/yaoyy/ms-biz/src/main/java/com/ms/biz/shiro/ShiroConfiguration.java
@@ -103,6 +103,7 @@ public class ShiroConfiguration {
 
         shiroFilterFactoryBean.setFilterChainDefinitionsString("/user/login=anon;" +
                 "/user/logout=logout;" +
+                "/user/updatePassword=bizAuthorization;" +
                 "/apply/sample=anon;"  +
                 "/assets/**=anon;" +
                 "/error/**=anon;" +
