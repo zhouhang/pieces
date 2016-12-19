62246822f477e40958bf1843e15d2e211d10d831
chaochaoxiao <376161552@qq.com>
2016-12-19 17:31:13 +0800

    sql备份

--- res/pieces/v1.4.sql
+++ res/pieces/v1.4.sql
@@ -30,5 +30,22 @@ WHILE
     END WHILE;
 CLOSE cur;
 END
-
 call insertdata();
+
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL COMMENT '用户id',
+  `pay_type` int(5) DEFAULT NULL COMMENT '支付方式',
+  `status` int(5) DEFAULT NULL COMMENT '支付状态',
+  `money` decimal(2,0) DEFAULT NULL COMMENT '支付金额',
+  `order_id` int(11) DEFAULT NULL COMMENT '订单id',
+  `trade_no` varchar(64) DEFAULT NULL COMMENT '支付宝/微信交易号',
+  `out_trade_no` varchar(255) DEFAULT NULL COMMENT '提供给第三方支付的订单号，必须唯一',
+  `remark` varchar(255) DEFAULT NULL,
+  `in_param` text COMMENT '传入参数',
+  `out_param` text COMMENT '返回参数',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `callback_time` datetime DEFAULT NULL COMMENT '回调时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='线上支付表';
