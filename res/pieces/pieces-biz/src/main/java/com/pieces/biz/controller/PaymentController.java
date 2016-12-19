e4186ff38fe1dd822dd416108ae6c21b4a5b82d3
wangbin <burgleaf@gmail.com>
2016-12-19 16:30:23 +0800

    提交支付宝接入demo

---
+++ res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/PaymentController.java
@@ -0,0 +1,185 @@
+package com.pieces.biz.controller;
+
+import com.pieces.service.pay.alipay.AlipayNotify;
+import com.pieces.service.pay.alipay.AlipaySubmit;
+import com.pieces.service.pay.config.AlipayConfig;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by wangbin on 2016/12/19.
+ */
+@Controller
+@RequestMapping("/pay")
+public class PaymentController extends BaseController{
+
+
+    @RequestMapping(value = "alipay/test/index")
+    public String payTestIndex(){
+        return "alipay_test_index";
+    }
+
+
+    @RequestMapping(value = "alipay/test" )
+    public void payTest(@RequestParam(required = false,value = "WIDout_trade_no") String out_trade_no,
+                          @RequestParam(required = false,value = "WIDsubject") String subject,
+                          @RequestParam(required = false,value = "WIDtotal_fee") String total_fee,
+                          @RequestParam(required = false,value = "WIDbody") String body,
+                        HttpServletResponse response)throws Exception{
+        Map<String, String> sParaTemp = new HashMap<String, String>();
+        sParaTemp.put("service", AlipayConfig.service);
+        sParaTemp.put("partner", AlipayConfig.partner);
+        sParaTemp.put("seller_id", AlipayConfig.seller_id);
+        sParaTemp.put("_input_charset", AlipayConfig.input_charset);
+        sParaTemp.put("payment_type", AlipayConfig.payment_type);
+        sParaTemp.put("notify_url", AlipayConfig.notify_url);
+        sParaTemp.put("return_url", AlipayConfig.return_url);
+        sParaTemp.put("anti_phishing_key", AlipayConfig.anti_phishing_key);
+        sParaTemp.put("exter_invoke_ip", AlipayConfig.exter_invoke_ip);
+        sParaTemp.put("out_trade_no", out_trade_no);
+        sParaTemp.put("subject", subject);
+        sParaTemp.put("total_fee", total_fee);
+        sParaTemp.put("body", body);
+        String htmlOut = AlipaySubmit.buildRequest(sParaTemp,"get","确认");
+        System.out.println(htmlOut);
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println(htmlOut);
+    }
+
+
+
+    @RequestMapping(value = "alipay/result/page" )
+    public String resutlPage( HttpServletResponse response,
+                              HttpServletRequest request,
+                              ModelMap modelMap)throws Exception{
+        //获取支付宝GET过来反馈信息
+        Map<String,String> params = new HashMap<String,String>();
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i]
+                        : valueStr + values[i] + ",";
+            }
+            //乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+//            valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+            params.put(name, valueStr);
+        }
+
+        //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+        //商户订单号
+        String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+        //支付宝交易号
+        String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
+
+        //交易状态
+        String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+
+        //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+
+        //计算得出通知验证结果
+        boolean verify_result = AlipayNotify.verify(params);
+
+        if(verify_result){//验证成功
+            //////////////////////////////////////////////////////////////////////////////////////////
+            //请在这里加上商户的业务逻辑程序代码
+            //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+            if(trade_status.equals("TRADE_FINISHED") || trade_status.equals("TRADE_SUCCESS")){
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+            }
+            //该页面可做页面美工编辑
+            modelMap.put("result","验证成功");
+            //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+            //////////////////////////////////////////////////////////////////////////////////////////
+        }else{
+            //该页面可做页面美工编辑
+            modelMap.put("result","验证失败");
+        }
+        return "alipay_test_result";
+    }
+
+
+
+    @RequestMapping(value = "alipay/result/notify" )
+    public void resutlNotify(HttpServletResponse response,
+                             HttpServletRequest request,
+                             ModelMap modelMap)throws Exception{
+        //获取支付宝POST过来反馈信息
+        Map<String,String> params = new HashMap<String,String>();
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i]
+                        : valueStr + values[i] + ",";
+            }
+            //乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+            //valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
+            params.put(name, valueStr);
+        }
+
+        //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+        //商户订单号
+
+        String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+
+        //支付宝交易号
+
+        String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
+
+        //交易状态
+        String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+
+        //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+
+        if(AlipayNotify.verify(params)){//验证成功
+            //////////////////////////////////////////////////////////////////////////////////////////
+            //请在这里加上商户的业务逻辑程序代码
+
+            //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+
+            if(trade_status.equals("TRADE_FINISHED")){
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
+                //如果有做过处理，不执行商户的业务程序
+
+                //注意：
+                //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+            } else if (trade_status.equals("TRADE_SUCCESS")){
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
+                //如果有做过处理，不执行商户的业务程序
+
+                //注意：
+                //付款完成后，支付宝系统发送该交易状态通知
+            }
+
+            //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+            System.out.print("success");	//请不要修改或删除
+
+            //////////////////////////////////////////////////////////////////////////////////////////
+        }else{//验证失败
+            System.out.printf("fail");
+        }
+
+
+    }
+}
---
+++ res/pieces/pieces-biz/src/main/webapp/public/alipay_test_result.ftl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<#include "./inc/meta.ftl"/>
+    <meta charset="UTF-8">
+    <title>支付宝回调成功</title>
+</head>
+
+<body>
+    <h1>支付结果</h1>
+    <p>${result!}</p>
+</body>
+</html>
