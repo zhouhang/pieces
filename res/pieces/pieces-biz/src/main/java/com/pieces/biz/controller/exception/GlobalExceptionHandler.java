23bbe39a16ffa3abd7add42d2bd7888b4c19e8fa
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2017-02-07 14:29:03 +0800

    [Summary]资源文件MD5

--- res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/exception/GlobalExceptionHandler.java
+++ res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/exception/GlobalExceptionHandler.java
@@ -5,12 +5,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.ResourceUrlProvider;
 
 /**
  * Created by wangbin on 2016/6/30.
@@ -18,6 +21,14 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 public class GlobalExceptionHandler  extends BaseGlobalExceptionHandler{
 
+    @Autowired
+    private ResourceUrlProvider resourceUrlProvider;
+
+
+    @ModelAttribute("urls")
+    ResourceUrlProvider urls() {
+        return this.resourceUrlProvider;
+    }
 
     //404的异常就会被这个方法捕获
     @ExceptionHandler(NoHandlerFoundException.class)
