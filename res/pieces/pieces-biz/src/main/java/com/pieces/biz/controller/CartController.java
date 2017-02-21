d19aaaad7f37864dfbfd52d9e915b4cdba4b9fbf
chaochaoxiao <376161552@qq.com>
2017-02-21 16:20:21 +0800

    询价单页面完成

--- res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/CartController.java
+++ res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/CartController.java
@@ -3,6 +3,7 @@ package com.pieces.biz.controller;
 import com.pieces.service.CartsCommodityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -25,7 +26,10 @@ public class CartController {
        CartsCommodityService cartsCommodityService;
 
 
-
+       @RequestMapping("/index")
+       public String index(ModelMap modelMap){
+              return "cart_index";
+       }
 
 
 
