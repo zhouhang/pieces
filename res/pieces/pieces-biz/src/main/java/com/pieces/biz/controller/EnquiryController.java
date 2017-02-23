0f7aa913bb31142abada12a03c71ea3f2fb01636
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2017-02-23 17:16:46 +0800

    [Summary]删除原来的匿名询价和询价功能

--- res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/EnquiryController.java
+++ res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/EnquiryController.java
@@ -3,32 +3,24 @@ package com.pieces.biz.controller;
 import com.github.pagehelper.PageInfo;
 import com.pieces.biz.controller.commons.LogConstant;
 import com.pieces.dao.elasticsearch.document.CommodityDoc;
-import com.pieces.dao.model.EnquiryBills;
 import com.pieces.dao.model.EnquiryCommoditys;
 import com.pieces.dao.model.User;
-import com.pieces.dao.vo.CommodityVo;
 import com.pieces.dao.vo.EnquiryBillsVo;
 import com.pieces.dao.vo.EnquiryRecordVo;
-import com.pieces.service.*;
-import com.pieces.service.constant.BasicConstants;
+import com.pieces.service.CommoditySearchService;
+import com.pieces.service.EnquiryBillsService;
+import com.pieces.service.EnquiryCommoditysService;
+import com.pieces.service.UserService;
 import com.pieces.service.constant.bean.Result;
 import com.pieces.service.dto.UserValidate;
-import com.pieces.service.enums.NotifyTemplateEnum;
 import com.pieces.service.enums.RedisEnum;
-import com.pieces.service.listener.NotifyEvent;
 import com.pieces.service.utils.ExcelParse;
-import com.pieces.tools.annotation.SecurityToken;
 import com.pieces.tools.log.annotation.BizLog;
-import com.pieces.tools.utils.CookieUtils;
-import com.pieces.tools.utils.GsonUtil;
-import com.pieces.tools.utils.SpringUtil;
 import com.pieces.tools.utils.WebUtil;
-import org.apache.commons.lang.StringUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,8 +30,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.List;
 
 /**
  * 询价前端
@@ -49,10 +40,6 @@ import java.util.*;
 @RequestMapping(value = "/center/enquiry")
 public class EnquiryController extends BaseController{
 
-    private static final int COOKIE_EXPIRE = 3600;
-
-    @Autowired
-    private CommodityService commodityService;
     @Autowired
     private EnquiryBillsService enquiryBillsService;
     @Autowired
@@ -65,106 +52,9 @@ public class EnquiryController extends BaseController{
     private HttpSession httpSession;
 
 
-    /**
-     * 客户询价页面
-     */
-    @RequestMapping(value = "index")
-    @BizLog(type = LogConstant.enquiry, desc = "客户询价页面")
-    @SecurityToken(generateToken = true)
-    public String index(HttpServletRequest request,
-                        HttpServletResponse response,
-                        ModelMap modelMap,
-                        Integer billId,
-                        Integer commodityId)throws  Exception{
-        //重新询价删除之前的询价记录
-        if(billId!=null){
-            User user = (User) request.getSession().getAttribute(RedisEnum.USER_SESSION_BIZ.getValue());
-            List<EnquiryCommoditys> enquiryCommoditysList = handleEnquiryAgain(user.getId(),billId);
-            if(!enquiryCommoditysList.isEmpty()){
-                modelMap.put("enquiryCommoditysList",enquiryCommoditysList);
-                modelMap.put("billId",billId);
-                return "user_enquiry";
-            }
-        }
-
-        Set<Integer> cookieSet = null;
-        String cookieValue = CookieUtils.getCookieValue(request, BasicConstants.ENQUIRY_COOKIES);
-        if(StringUtils.isBlank(cookieValue)){
-            cookieSet = new HashSet<>();
-        }else{
-            cookieSet = GsonUtil.jsonToEntity(cookieValue,Set.class);
-        }
-        if(commodityId!=null){
-            cookieSet.add(commodityId);
-        }
-
-        //把询价的商品ID转换成集合对象
-        List<CommodityVo> list = new ArrayList<>();
-        if(!cookieSet.isEmpty()){
-            list = commodityService.findVoByIds(cookieSet);
-        }
-
-        CookieUtils.setCookie(response, BasicConstants.ENQUIRY_COOKIES, GsonUtil.toJson(cookieSet),COOKIE_EXPIRE);
-        modelMap.put("commodityList",list);
-        return "user_enquiry";
-    }
-
-
     private List<EnquiryCommoditys> handleEnquiryAgain(Integer userId,Integer billId){
         List<EnquiryCommoditys> enquiryCommoditysList =  enquiryCommoditysService.findByBillId(billId,userId,null);
         return enquiryCommoditysList;
-    }
-
-
-    /**
-     * 删除询价单里的一个商品
-     */
-    @RequestMapping(value = "delete")
-    public void delete(HttpServletRequest request,
-                       HttpServletResponse response,
-                       Integer commodityId)throws Exception{
-        String cookieValue = CookieUtils.getCookieValue(request, BasicConstants.ENQUIRY_COOKIES);
-        Set<Integer> cookieSet = GsonUtil.jsonToEntity(cookieValue,Set.class);
-        if(!cookieSet.isEmpty()){
-            cookieSet.remove(commodityId);
-        }
-        CookieUtils.setCookie(response, BasicConstants.ENQUIRY_COOKIES,GsonUtil.toJson(cookieSet),COOKIE_EXPIRE);
-        WebUtil.print(response,new Result(true));
-    }
-
-    /**
-     * 提交询价单
-     */
-    @RequestMapping(value = "submit")
-    @BizLog(type = LogConstant.enquiry, desc = "提交询价单")
-    @SecurityToken(validateToken=true)
-    public void submit(HttpServletRequest request,
-                       HttpServletResponse response,
-                       Integer billId,
-                       @RequestBody List<EnquiryCommoditys> list)throws Exception{
-            String message = "您的询价提交成功!";
-            User user = (User) request.getSession().getAttribute(RedisEnum.USER_SESSION_BIZ.getValue());
-            if(billId==null){
-                billId = enquiryBillsService.create(list,user);
-            }else{
-                try {
-                    message="您的询价单重新修改成功!";
-                    enquiryBillsService.update(list,user,billId);
-                }catch (Exception e){
-                    e.printStackTrace();
-                    WebUtil.print(response,new Result(false).info(e.getMessage()));
-                    return;
-                }
-            }
-        //删除之前的询价记录
-        CookieUtils.deleteCookie(request,response, BasicConstants.ENQUIRY_COOKIES);
-
-        //用户询价成功后通知管理员处理
-        SimpleDateFormat time=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        SpringUtil.getApplicationContext().
-                publishEvent(new NotifyEvent(NotifyTemplateEnum.enquiry.getTitle(String.valueOf(billId)),
-                        NotifyTemplateEnum.enquiry.getContent(user.getContactName(),time.format(new Date())),NotifyTemplateEnum.enquiry.getType(),billId));
-        WebUtil.print(response,new Result(true).info(message));
     }
 
     /**
