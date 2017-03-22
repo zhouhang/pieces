32fdb8fa09caad6562e187fe985e63118f687c70
xiao <376161552@qq.com>
2017-03-22 10:33:52 +0800

    文章详情seo信息

--- res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/HelpController.java
+++ res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/HelpController.java
@@ -1,9 +1,13 @@
 package com.pieces.biz.controller;
 
 import com.pieces.biz.controller.commons.LogConstant;
+import com.pieces.dao.enums.SeoTypeEnum;
 import com.pieces.dao.model.Article;
+import com.pieces.dao.model.ArticleCategory;
+import com.pieces.dao.model.SeoSetting;
 import com.pieces.dao.vo.ArticleCategoryVo;
 import com.pieces.service.ArticleService;
+import com.pieces.service.SeoSettingService;
 import com.pieces.service.enums.ModelEnum;
 import com.pieces.tools.log.annotation.BizLog;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +28,9 @@ public class HelpController extends BaseController{
     @Autowired
     ArticleService articleService;
 
+    @Autowired
+    private SeoSettingService seoSettingService;
+
     @RequestMapping("/{id}")
     @BizLog(type = LogConstant.help, desc = "帮助中心页面")
     public String detail(@PathVariable("id") Integer id, ModelMap model) {
@@ -33,6 +40,39 @@ public class HelpController extends BaseController{
         model.put("articleCategorylist",articleCategorylist);
         model.put("article",article);
         // 回填帮助中心前台
+
+        //seo信息
+        SeoSetting base=seoSettingService.findByType(SeoTypeEnum.BASE.getValue());
+        SeoSetting commditySetting=seoSettingService.findByType(SeoTypeEnum.ARTICLE_DETAIL.getValue());
+        String title=commditySetting.getTitle();
+        if(title!=null){
+            title=title.replace("{文章标题}",article.getTitle());
+            ArticleCategory articleCategory=articleService.getCategoryById(article.getCategoryId());
+            title=title.replace("{分类名称}",articleCategory.getName());
+            title=title.replace("{通用标题}",base.getTitle()==null?"":base.getTitle());
+        }
+        String description=commditySetting.getIntro();
+        if(description!=null){
+            description=description.replace("{文章描述}",article.getIntro()==null?"":article.getIntro());
+            description=description.replace("{通用描述}",base.getIntro()==null?"":base.getIntro());
+        }
+
+
+
+        String keyWords=commditySetting.getKeyWord();
+        if(keyWords!=null){
+            keyWords=keyWords.replace("{文章关键字}",article.getKeyWord()==null?"":article.getKeyWord());
+            keyWords=keyWords.replace("{通用关键字}",base.getKeyWord()==null?"":base.getKeyWord());
+        }
+
+
+
+        model.put("title",title);
+        model.put("description",description);
+        model.put("keyWords",keyWords);
+
+
+
         return "helper";
     }
 }
--- res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/NewsController.java
+++ res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/NewsController.java
@@ -3,9 +3,13 @@ package com.pieces.biz.controller;
 import com.github.inspektr.audit.annotation.Audit;
 import com.github.pagehelper.PageInfo;
 import com.pieces.biz.controller.commons.LogConstant;
+import com.pieces.dao.enums.SeoTypeEnum;
 import com.pieces.dao.model.Article;
+import com.pieces.dao.model.ArticleCategory;
+import com.pieces.dao.model.SeoSetting;
 import com.pieces.dao.vo.ArticleVo;
 import com.pieces.service.ArticleService;
+import com.pieces.service.SeoSettingService;
 import com.pieces.service.enums.ModelEnum;
 import com.pieces.tools.log.annotation.BizLog;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +31,11 @@ public class NewsController extends BaseController {
     @Autowired
     ArticleService articleService;
 
+    @Autowired
+    private SeoSettingService seoSettingService;
+
+
+
     @RequestMapping("/{id}")
     @BizLog(type = LogConstant.logistics, desc = "新闻中心")
     public String detail(@PathVariable("id") Integer id, ModelMap model) {
@@ -38,6 +47,37 @@ public class NewsController extends BaseController {
         // 回填前台显示
         model.put("articleList",articleList);
         model.put("article",article);
+        //seo信息
+        SeoSetting base=seoSettingService.findByType(SeoTypeEnum.BASE.getValue());
+        SeoSetting commditySetting=seoSettingService.findByType(SeoTypeEnum.ARTICLE_DETAIL.getValue());
+        String title=commditySetting.getTitle();
+        if(title!=null){
+            title=title.replace("{文章标题}",article.getTitle());
+            ArticleCategory articleCategory=articleService.getCategoryById(article.getCategoryId());
+            title=title.replace("{分类名称}",articleCategory.getName());
+            title=title.replace("{通用标题}",base.getTitle()==null?"":base.getTitle());
+        }
+        String description=commditySetting.getIntro();
+        if(description!=null){
+            description=description.replace("{文章描述}",article.getIntro()==null?"":article.getIntro());
+            description=description.replace("{通用描述}",base.getIntro()==null?"":base.getIntro());
+        }
+
+
+
+        String keyWords=commditySetting.getKeyWord();
+        if(keyWords!=null){
+            keyWords=keyWords.replace("{文章关键字}",article.getKeyWord()==null?"":article.getKeyWord());
+            keyWords=keyWords.replace("{通用关键字}",base.getKeyWord()==null?"":base.getKeyWord());
+        }
+
+
+
+        model.put("title",title);
+        model.put("description",description);
+        model.put("keyWords",keyWords);
+
+
         return "article";
     }
 
