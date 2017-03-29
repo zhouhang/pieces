f311f1307e204e5b5af4dd5fc1df28b2e88edca7
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2017-03-29 10:57:29 +0800

    [Summary]

---
+++ res/pieces/pieces-biz/src/main/java/com/pieces/biz/controller/H5OrderController.java
@@ -0,0 +1,48 @@
+package com.pieces.biz.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.google.common.base.Strings;
+import com.pieces.biz.shiro.BizToken;
+import com.pieces.dao.model.EnquiryCommoditys;
+import com.pieces.dao.model.User;
+import com.pieces.dao.vo.*;
+import com.pieces.service.*;
+import com.pieces.service.constant.bean.Result;
+import com.pieces.service.enums.RedisEnum;
+import com.pieces.service.redis.RedisManager;
+import com.pieces.service.vo.CropResult;
+import com.pieces.tools.bean.BASE64DecodedMultipartFile;
+import com.pieces.tools.exception.NotFoundException;
+import com.pieces.tools.utils.WebUtil;
+import me.chanjar.weixin.common.bean.WxJsapiSignature;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import sun.misc.BASE64Decoder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * Author: koabs
+ * 3/14/17.
+ * 微信端订单功能
+ */
+@Controller
+@RequestMapping("/h5/")
+public class H5OrderController {
+
+    Logger logger = LoggerFactory.getLogger(H5OrderController.class);
+
+
+}
