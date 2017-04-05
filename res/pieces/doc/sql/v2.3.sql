653980ab7a025bce22adfff7bb00510683b3f3f9
xiao <376161552@qq.com>
2017-04-05 17:44:59 +0800

    后台数据库

--- res/pieces/doc/sql/v2.3.sql
+++ res/pieces/doc/sql/v2.3.sql
@@ -9,3 +9,15 @@ CREATE TABLE `user_follow_record` (
   `result` text COMMENT '跟进结果',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+#接入快递api
+CREATE TABLE `logistical_trace` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `logistic_code` varchar(50) NOT NULL COMMENT '快递单号',
+  `shipper_code` varchar(10) NOT NULL COMMENT '快递公司编码',
+  `accept_station` text NOT NULL COMMENT '轨迹内容',
+  `accept_time` datetime NOT NULL COMMENT '轨迹时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
