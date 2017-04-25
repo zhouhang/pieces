6a3227fa984ca0b0282155d09cb1e2bebe0ede4b
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2017-04-25 16:28:35 +0800

    [Summary]更新前台橱窗商品

--- res/pieces/doc/sql/v2.3.sql
+++ res/pieces/doc/sql/v2.3.sql
@@ -21,3 +21,13 @@ CREATE TABLE `logistical_trace` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+
+# 更新首页橱窗商品
+UPDATE `pieces`.`home_weight` SET `value`='1065,310,694,687,1485,1498,1824,542' WHERE `id`='91'; -- 根茎类
+UPDATE `pieces`.`home_weight` SET `value`='267,710,886,1329,1380,1168,203,275' WHERE `id`='101'; -- 果实籽仁类
+UPDATE `pieces`.`home_weight` SET `value`='1169,1725,1807,444,1907,494,183,1258' WHERE `id`='111'; -- 全草花叶类
+UPDATE `pieces`.`home_weight` SET `value`='484,233,405,1546,1944,831,185,883' WHERE `id`='122'; -- 矿石动物类
+UPDATE `pieces`.`home_weight` SET `value`='126,431,646,699,1884,1471,553,1022' WHERE `id`='131'; -- 藤皮类
+UPDATE `pieces`.`home_weight` SET `value`='975,489,634,623,487,1873,492,249' WHERE `id`='141'; --树脂菌藻类
+
+
