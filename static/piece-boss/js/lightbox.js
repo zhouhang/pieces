cd4aaca561d09203cfc6d65a0bd04a16329c05a7
frank <frank1989678@126.com>
2017-05-03 15:07:12 +0800

    图片展示插件添加旋转功能

--- static/piece-boss/js/lightbox.js
+++ static/piece-boss/js/lightbox.js
@@ -49,7 +49,7 @@
 		},
 		build: function() {
 			var self = this;
-			$('<div id="lightboxOverlay" class="lightboxOverlay"></div><div id="lightbox" class="lightbox"><div class="lb-nav"></div><div class="lb-thumb"><img class="lb-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/><span class="lb-prev"></span><span class="lb-next"></span></div><div class="lb-loader"></div><div class="lb-close" title="关闭"></div></div>').appendTo($('body'));
+			$('<div id="lightboxOverlay" class="lightboxOverlay"></div><div id="lightbox" class="lightbox"><div class="lb-nav"></div><div class="lb-thumb"><img class="lb-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/><span class="lb-prev" title="上一张"></span><span class="lb-next" title="下一张"></span></div><div class="lb-loader"></div><div class="lb-close" title="关闭"></div><div class="lb-op"><button type="button" class="lb-turn-left">左转</button><button type="button" class="lb-turn-right">右转</button></div></div>').appendTo($('body'));
 			this.$lightbox = $('#lightbox');
 			this.$overlay = $('#lightboxOverlay');
 			self.$image = this.$lightbox.find('.lb-image');
@@ -79,12 +79,97 @@
 				}
 				return false
 			});
+			this.$lightbox.on('click', '.lb-turn-left', function() {			
+				self.rotate(-1);
+				return false;
+			});
+			this.$lightbox.on('click', '.lb-turn-right', function() {				
+				self.rotate(1);
+				return false;
+			});
 			return this;
+		},
+		rotate: function(arrow) {
+			var self = this;
+			var img = new Image();
+			img.onload = function() {
+				var imageHeight = img.height; 
+				var imageWidth = img.width;		
+				var windowHeight = self.windowHeight - 40;
+				var windowWidth = self.windowWidth - 20;
+				var maxImageHeight = self.options.maxHeight || windowHeight; 
+				var maxImageWidth = self.options.maxWidth || windowWidth; 
+				var rotation = self.rotation;
+				var pos = 0;
+				img.onload = null;
+
+				maxImageHeight = Math.min(maxImageHeight, windowHeight);
+				maxImageWidth = Math.min(maxImageWidth, windowWidth);
+
+				rotation += arrow;
+
+				if (rotation === 4) {
+					rotation = 0;
+				} else if (rotation === -1) {
+					rotation = 3;
+				}
+
+				if (rotation % 2 !== 0) {
+					var temp = imageWidth;
+					imageWidth = imageHeight;
+					imageHeight = temp;
+				}
+				self.rotation = rotation;
+
+				if ((imageWidth > maxImageWidth) || (imageHeight > maxImageHeight)) {
+					if ((imageWidth / maxImageWidth) > (imageHeight / maxImageHeight)) {
+						imageHeight = parseInt(imageHeight * maxImageWidth / imageWidth, 10);
+						imageWidth = maxImageWidth;
+					} else {
+						imageWidth = parseInt(imageWidth * maxImageHeight / imageHeight, 10);
+						imageHeight = maxImageHeight;
+					}
+				}
+
+				imageWidth = Math.max(300, imageWidth);
+				imageHeight = Math.max(300, imageHeight);
+
+				self.$lightbox.css({
+					width: imageWidth,
+					height: imageHeight,
+					marginLeft: -(imageWidth+8)/2,
+					marginTop: -(imageHeight+8)/2
+				});
+
+				if (rotation % 2 !== 0) {
+					pos = (imageWidth - imageHeight)/2;
+					self.$image.css({
+						width: imageHeight,
+						height: imageWidth
+					})
+				} else {
+					self.$image.css({
+						width: imageWidth,
+						height: imageHeight
+					})
+				}
+				self.$image.css({
+					'transform': 'rotate(' + (90*rotation) + 'deg)',
+					'WebkitTransform': 'rotate(' + (90*rotation) + 'deg)',
+					'OTransform': 'rotate(' + (90*rotation) + 'deg)', 
+					'msTransform': 'rotate(' + (90*rotation) + 'deg)', 
+					'MozTransform': 'rotate(' + (90*rotation) + 'deg)',
+					'marginLeft': pos + 'px',
+					'marginTop': -pos + 'px'
+				})
+			};
+			img.src = self.album[self.currentImageIndex].url;
+			return false
 		},
 		start: function($img) {
 			var self = this;
 			self.album = [];
-			$img.parent().find('img').each(function() {
+			$img.closest(self.options.selector).find('img').each(function() {
 				self.album.push({
 					url: $(this).data('src') || this.src,
 					preloader: false
@@ -104,7 +189,7 @@
 			img.onload = function() {
 				var imageHeight = img.height; 
 				var imageWidth = img.width;		
-				var windowHeight = self.windowHeight - 120;
+				var windowHeight = self.windowHeight - 40;
 				var windowWidth = self.windowWidth - 20;
 				var maxImageHeight = self.options.maxHeight || windowHeight; 
 				var maxImageWidth = self.options.maxWidth || windowWidth; 
@@ -129,8 +214,8 @@
 				self.$lightbox.animate({
 					width: imageWidth,
 					height: imageHeight,
-					marginLeft: -(imageWidth)/2,
-					marginTop: -(imageHeight)/2
+					marginLeft: -(imageWidth+8)/2,
+					marginTop: -(imageHeight+8)/2
 				}, self.options.fadeDuration, 'swing', function() {
 					self.$image.attr({
 						'src': self.album[current].url,
@@ -144,6 +229,8 @@
 			img.src = self.album[current].url;
 			self.currentImageIndex = current;
 			self.preloadNeighboringImages();
+			self.rotation = 0;
+			self.rotate(0);
 			self.showNav();
 
 		},
